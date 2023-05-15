0c50995229c54f28bb486d7dc1f1aec7f385ad4f
Logan Bailey <logan.n.bailey@gmail.com>
2023-05-15 22:41:55 +0000

    sendMessage: tighten message validation and surface fetch errors

--- helpers/sendMessage.ts
+++ helpers/sendMessage.ts
@@ -2,31 +2,33 @@ import encrypt from "../web3/cryptography/encrypt";
 
 const POST_CONVO_ENDPOINT = "/api/postConvo";
 
+const isNonEmptyString = (value) =>
+  typeof value === "string" && value.trim().length > 0;
+
 const sendMessage = async (convo, currentMessage) => {
   if (!convo || typeof convo.tokenID === "undefined") {
     throw new Error("sendMessage: missing convo or tokenID");
   }
-  if (!currentMessage) {
+  if (!isNonEmptyString(currentMessage)) {
     throw new Error("sendMessage: message is empty");
   }
 
-  try {
-    const response = await fetch(POST_CONVO_ENDPOINT, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        tokenID: convo.tokenID,
-        message: currentMessage,
-      }),
-    });
+  const payload = JSON.stringify({
+    tokenID: convo.tokenID,
+    message: currentMessage,
+  });
 
-    if (!response.ok) {
-      throw new Error(`Failed to send message (status ${response.status})`);
-    }
+  const response = await fetch(POST_CONVO_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: payload,
+  });
 
-    return response;
-  } catch (err) {
-    alert(err.message);
+  if (!response.ok) {
+    throw new Error(`Failed to send message (status ${response.status})`);
   }
+
+  return response;
 };
+
 export default sendMessage;
