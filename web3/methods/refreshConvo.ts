3953500e4cbc5c989741045681d2d502f818f424
Logan Bailey <logan.n.bailey@gmail.com>
2023-05-23 19:49:11 +0000

    Refactor refreshConvo: extract mapper and validate inputs

--- web3/methods/refreshConvo.ts
+++ web3/methods/refreshConvo.ts
@@ -1,15 +1,23 @@
 import { Conversation } from "../../interfaces";
 import conversations from "../../conversations.json";
+
+const mapConversation = ({ secretHash, tokenID, IPFSendpoint }: Conversation) => ({
+  secretHash,
+  tokenID,
+  IPFSendpoint,
+  messages: conversations[tokenID] ?? [],
+});
+
 const refreshConvo = async (contract, wallet) => {
-  const activeConversations: Conversation[] = await contract.methods // get all active conversations
+  if (!contract || !wallet) {
+    throw new Error("refreshConvo requires both a contract instance and a wallet address");
+  }
+
+  const activeConversations: Conversation[] = await contract.methods
     .getMyActiveConversations(wallet)
     .call({ from: wallet });
 
-  return activeConversations.map(({ secretHash, tokenID, IPFSendpoint }) => ({
-    secretHash,
-    tokenID,
-    IPFSendpoint,
-    messages: conversations[tokenID],
-  }));
+  return activeConversations.map(mapConversation);
 };
+
 export default refreshConvo;
