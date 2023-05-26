9ff41dffb42e8a7aa3ea84ec0d0c039082d4f8aa
Logan Bailey <logan.n.bailey@gmail.com>
2023-05-26 18:48:47 +0000

    Add type annotations to decrypt parameters

--- web3/cryptography/decrypt.ts
+++ web3/cryptography/decrypt.ts
@@ -1,9 +1,9 @@
-const decrypt = (secretHash, encryptedMessage) => {
-  const decipher = (salt) => {
-    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-    const applySaltToChar = (code) =>
+const decrypt = (secretHash: string, encryptedMessage: string): string => {
+  const decipher = (salt: string) => {
+    const textToChars = (text: string) => text.split("").map((c) => c.charCodeAt(0));
+    const applySaltToChar = (code: number) =>
       textToChars(salt).reduce((a, b) => a ^ b, code);
-    return (encoded) =>
+    return (encoded: string) =>
       encoded
         .match(/.{1,2}/g)
         .map((hex) => parseInt(hex, 16))
