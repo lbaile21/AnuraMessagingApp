3bc1c09a8edba044edae5dd024bc7ec230b63e75
Logan Bailey <logan.n.bailey@gmail.com>
2023-09-11 23:34:58 +0000

    docs(postConvo): document endpoint contract and clean up comments

--- pages/api/postConvo.ts
+++ pages/api/postConvo.ts
@@ -1,24 +1,41 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import previousConversations from "../../conversations.json";
 import fs from "fs";
+
+/**
+ * POST /api/postConvo
+ *
+ * Persists a single conversation entry, keyed by `tokenID`, into the
+ * `conversations.json` store at the project root. The endpoint merges the
+ * incoming `message` with any previously stored conversations so that
+ * existing entries are preserved (or overwritten if the same `tokenID` is
+ * supplied again).
+ *
+ * Expected request body (JSON-encoded string):
+ *   {
+ *     "tokenID": string,   // unique identifier for the conversation
+ *     "message": unknown   // payload to associate with the tokenID
+ *   }
+ *
+ * Response: the current size of `conversations.json` in megabytes, which
+ * callers can use as a lightweight signal for storage growth.
+ */
 const postConversation = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    // grab token id and cid from request
+    // Grab tokenID and message from the request body.
     const { tokenID, message } = JSON.parse(req.body);
-    // spread previous conversations into current conversations
+
+    // Merge the new entry into the existing conversations map.
     const conversations = { ...previousConversations };
-    // add or change current conversation's
     conversations[tokenID] = message;
-    // write to string
     const jsonString = JSON.stringify(conversations);
-    //  get stats of our conversations files
+
+    // Measure the on-disk size of the store before writing the update.
     const stats = fs.statSync(`./conversations.json`);
-    // get size in bytes
     const fileSizeInBytes = stats.size;
-    // get size in MB
     const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
 
-    // write to json file
+    // Persist the updated conversations map back to disk.
     fs.writeFile(`./conversations.json`, jsonString, (err) => {
       if (err) {
         console.log("Error writing file", err);
@@ -26,7 +43,8 @@ const postConversation = async (req: NextApiRequest, res: NextApiResponse) => {
         console.log("Successfully wrote file");
       }
     });
-    // return size in MB
+
+    // Return the pre-write file size (in MB) to the caller.
     res.json(fileSizeInMegabytes);
   } catch (err) {
     res.status(err.status).json({ err: err.message });
