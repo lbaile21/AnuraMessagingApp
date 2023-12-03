3460450f26c0e4ec5dabe8aafeb8898c0d2aabea
Logan Bailey <logan.n.bailey@gmail.com>
2023-12-03 01:26:08 +0000

    Add optional timestamp field and isMessage type guard

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -14,4 +14,20 @@ export interface Message {
   receiver?: string;
   /** Optional ARIA label for assistive technologies. */
   ariaLabel?: string;
+  /** Optional unix timestamp (ms) indicating when the message was created. */
+  timestamp?: number;
+}
+
+/**
+ * Type guard that verifies whether an arbitrary value conforms to the
+ * {@link Message} interface shape at runtime.
+ */
+export function isMessage(value: unknown): value is Message {
+  if (!value || typeof value !== "object") return false;
+  const v = value as Record<string, unknown>;
+  if (typeof v.sender !== "string" || typeof v.message !== "string") return false;
+  if (v.receiver !== undefined && typeof v.receiver !== "string") return false;
+  if (v.ariaLabel !== undefined && typeof v.ariaLabel !== "string") return false;
+  if (v.timestamp !== undefined && typeof v.timestamp !== "number") return false;
+  return true;
 }
