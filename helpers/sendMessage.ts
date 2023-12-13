1ccad4949f1353d7287140343bc4cdd4ad325d75
Logan Bailey <logan.n.bailey@gmail.com>
2023-12-13 14:44:53 +0000

    Extract validation helpers and enforce max message length

--- helpers/sendMessage.ts
+++ helpers/sendMessage.ts
@@ -1,17 +1,31 @@
 import encrypt from "../web3/cryptography/encrypt";
 
 const POST_CONVO_ENDPOINT = "/api/postConvo";
+const MAX_MESSAGE_LENGTH = 4096;
 
 const isNonEmptyString = (value) =>
   typeof value === "string" && value.trim().length > 0;
 
-const sendMessage = async (convo, currentMessage) => {
+const assertValidConvo = (convo) => {
   if (!convo || typeof convo.tokenID === "undefined") {
     throw new Error("sendMessage: missing convo or tokenID");
   }
-  if (!isNonEmptyString(currentMessage)) {
+};
+
+const assertValidMessage = (message) => {
+  if (!isNonEmptyString(message)) {
     throw new Error("sendMessage: message is empty");
   }
+  if (message.length > MAX_MESSAGE_LENGTH) {
+    throw new Error(
+      `sendMessage: message exceeds ${MAX_MESSAGE_LENGTH} characters`
+    );
+  }
+};
+
+const sendMessage = async (convo, currentMessage) => {
+  assertValidConvo(convo);
+  assertValidMessage(currentMessage);
 
   const payload = JSON.stringify({
     tokenID: convo.tokenID,
